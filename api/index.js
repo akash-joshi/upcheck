ff08f8a9086d2a93ac8188042c74c91bd167eafd
Akash Joshi <akash.joshi@viit.ac.in>
2021-08-27 11:44:50 +0530

    feat: update cors

--- api/index.js
+++ api/index.js
@@ -2,7 +2,24 @@
 
 const axios = require("axios");
 
-module.exports = async (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = async (req, res) => {
   const { url } = req.query;
 
   if (!url) {
@@ -16,3 +33,5 @@ module.exports = async (req, res) => {
     res.status(400).send("not ok");
   }
 };
+
+module.exports = allowCors(handler)
